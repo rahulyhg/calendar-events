3443d6d654c56132f4402936227fbc1d77c9319d
Sanjeev Maharjan <sanjeev.maharjan@hotmail.com>
2017-01-19 13:00:13 +0545

    added comments on is_taken method

--- application/modules/Users/models/Users_mdl.php
+++ application/modules/Users/models/Users_mdl.php
@@ -5,8 +5,9 @@ class Users_mdl extends MX_Model
 {
 
     private $_action;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         parent::__construct();
     }
     
@@ -15,50 +16,54 @@ class Users_mdl extends MX_Model
     public function login()
     {
         $this->_action = new Login_mdl(); // set values
-
+        
         return $this->_action->isgood();
     }
 
     public function signup()
     {
-
         $this->_action = new signup_mdl();
         $check = $this->_action->is_taken();
         if ($check['answer'] == FALSE) {
-            return 
-        } 
-        else{
             $this->_action->insert_to_verify(); // set values
         }
-                                    
-        //
+        return $check['taken'];
     }
 }
 
+// the login model
 class Login_mdl extends Base_Model
 {
-    //override Base_Model variables
+    // override Base_Model variables
     protected $_table_name = 'members';
+
     protected $_primary_key = 'id';
+
     protected $_primary_filter = 'intval';
+
     protected $_order_by = 'id';
+
     protected $_rules = array();
+
     protected $_timestamps = TRUE;
 
     private $_loginby;
+
     private $_hash_pass;
+
     private $_errors;
-    
+
     public function __construct()
     {
         parent::__construct();
-    
-        //set the values of member variables.
+        
+        // set the values of member variables.
         $this->_loginby = $this->input->post('loginby');
         $this->_hash_pass = hash('sha512', config_item('encrypt_key8') . $this->input->post('password') . config_item('encrypt_key16') . 'ce-ncit' . config_item('encrypt_key32'));
     }
-    
-    public function isgood() {
+
+    public function isgood()
+    {
         // check if email or username is entered.
         // assuming '@' is only possible in email and not username
         if (strpos($this->_loginby, '@') === TRUE) {
@@ -91,24 +96,32 @@ class Login_mdl extends Base_Model
     }
 }
 
+// the signup model
 class Signup_mdl extends Base_Model
 {
-    //override Base_Model variables
+    // override Base_Model variables
     protected $_table_name = 'verifypending';
+
     protected $_primary_key = 'id';
+
     protected $_primary_filter = 'intval';
+
     protected $_order_by = 'id';
+
     protected $_rules = array();
+
     protected $_timestamps = TRUE;
-
-    //data for rows
+    
+    // data for rows
     private $_username;
+
     private $_email;
+
     private $_hashpass;
+
     private $_errors;
-
-
-    //insert the registration data into verifypending table
+    
+    // insert the registration data into verifypending table
     public function insert_to_verify()
     {
         // if username was entered
@@ -124,19 +137,20 @@ class Signup_mdl extends Base_Model
         $this->_hash_pass = hash('sha512', config_item('encrypt_key8') . $this->input->post('password') . config_item('encrypt_key16') . 'ce-ncit' . config_item('encrypt_key32'));
     }
     
-    //checks if username/email is already registered.
-    //@return array['answer', arrayOfTakenVals]
-    public function istaken() {
-        //for test only.. more code required
-        return array(
-            'answer' => FALSE,
-            'taken'=> ''
-        );
-        /*
-        return array(
-            'answer' => FALSE,
-            'taken' => 'username and password' //output will be used as "array['taken'] already taken" if not null
-        );
-        */
+    // checks if username/email is already registered.
+    // @return array['answer', arrayOfTakenVals]
+    public function istaken()
+    {
+        /* 
+         * //output will be used as "@return already taken" if not null
+         * Used as follows:
+         * return 'Username and email';
+         * return 'Username';
+         * return 'Email';
+         * return '';
+         */
+        
+        // for test only.. more code required
+        return '';
     }
 }
