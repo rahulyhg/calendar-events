a5604db7ec57acc6dcdfb93535f34e3895a196c1
Sanjeev Maharjan <sanjeev.maharjan@hotmail.com>
2017-01-27 19:00:06 +0545

    added simple todo

--- application/config/fb_config.php
+++ application/config/fb_config.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+// @TODO make sure the fb website is set to the correct url
 $config['app_id'] = '385804648438360';
 $config['app_secret'] = '0c87f39f7c5499fe61a661ad7feb3a66';
 
