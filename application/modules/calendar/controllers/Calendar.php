993441455a05fd1b293aa9a4529a4ae9e4f6e107
Sanjeev Maharjan <sanjeev.maharjan@hotmail.com>
2017-01-28 19:43:31 +0545

    added the calendar module presumed basic structure

---
+++ application/modules/calendar/controllers/Calendar.php
@@ -0,0 +1,16 @@
+﻿<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Copythis extends MX_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        echo "hello";
+    }
+}
